3542dc7e87d40d62ac2c5dfe9975ad9130bd08e8
jaaomarcos <joaomarcosdeveloper@gmail.com>
2017-06-13 12:15:59 -0300

    update files

--- GA.py
+++ GA.py
@@ -2,13 +2,14 @@ import numpy as np
 import random
 
 class GA(object):
-	def __init__(self, populacao, limite, instancias, placa):
+	def __init__(self, populacao, instancias, placa, mutation, crossover):
 		self.geracao = []
 		self.populacao = populacao
-		self.limite = limite
 		self.pecas = []
 		self.instancias = instancias
 		self.placa = placa
+		self.mutation = mutation
+		self.crossover = crossover
 		pass
 
 	def duplique(self):
@@ -18,20 +19,21 @@ class GA(object):
 		        peca = [i[1], i[0]]
 		        self.pecas.append(peca)
 		    pass
+
 	def preprocessing(self):
 		for i in range(0,len(self.pecas)):
 			qtd = (self.placa[0] / self.pecas[i][0]) * (self.placa[1] / self.pecas[i][1])
 			self.pecas[i].append(qtd)
 			pass
 		pass
-		print self.pecas
+		return np.matrix(self.pecas)
 
 	def generate(self):
 		for i in range(0,self.populacao):
 		    pop = random.sample(range(0, len(self.pecas)), len(self.pecas)) 
 		    self.geracao.append(pop)
-		return self.geracao
 		pass
+		return np.matrix(self.geracao)
 
 	def mutation(self):
 		pass
@@ -39,7 +41,7 @@ class GA(object):
 	def crossover(self):
 		pass
 
-	def roleta(self):
+	def roulette(self):
 		pass
 
 	def evaluation(self):
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from GA import GA
 
-populacao = 10
-limite = 10
+populacao = 100
+mutation = 0.1
+crossover = 0.25
 instancias = [[120,10],[130,23],[100,12]]
 placa = [250,250]
 
-ga = GA(populacao, limite, instancias, placa)
+ga = GA(populacao, instancias, placa, mutation, crossover)
 ga.duplique()
-ga.preprocessing()
+print ga.preprocessing()
 print ga.generate()

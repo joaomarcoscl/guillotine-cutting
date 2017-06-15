5e92337e70dd0c609032ec446f2ed2290d6170e5
jaaomarcos <joaomarcosdeveloper@gmail.com>
2017-06-15 17:58:12 -0300

    update files

--- app.py
+++ app.py
@@ -3,10 +3,22 @@ from GA import GA
 populacao = 100
 mutation = 0.1
 crossover = 0.25
-instancias = [[120,10],[130,23],[100,12]]
+instancias = [
+    [167,184],
+    [114,118],
+    [167,152],
+    [83,140],
+    [70,86],
+    [143,166],
+    [120,160],
+    [66,148],
+    [87,141],
+    [69,165]
+]
 placa = [250,250]
 
 ga = GA(populacao, instancias, placa, mutation, crossover)
 ga.duplique()
-print ga.preprocessing()
-print ga.generate()
+ga.preprocessing()
+ga.generate()
+ga.evaluation()
